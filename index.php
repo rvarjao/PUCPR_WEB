a42d9175f003c7af42172592ff26bef426f0715f
Ricardo Varjão <rvarjao@gmail.com>
2018-10-20 23:29:20 -0300

    novo_banco_de_dados

--- index.php
+++ index.php
@@ -3,33 +3,6 @@
 	session_start();
 ?>
 
-<?php
-	
-
-	// decode the JSON into an associative array
-	$result = verifica_login_senha("asd", "asd");
-	echo "$result";
-	
-	function verifica_login_senha($testa_login, $testa_senha){
-		
-		$str = file_get_contents('banco_de_dados/usuarios.json');
-		$json_decodificado = json_decode($str, true); 
-		
-		if (array_key_exists($testa_login, $json_decodificado) == false){
-			return "nao existe esse usuario";
-		}
-		// bool array_key_exists ( mixed $key , array $array )
-
-		// foreach ($json_decodificado["usuarios"] as $key => $value) {
-		// echo "key: $key<br>";
-		// $senha = $value["senha"];
-		// echo "$senha<br>";
-	}
-
-
-
-?>
-
 <!DOCTYPE html>
 <html>
 <head>
@@ -41,22 +14,50 @@
 <meta charset="utf-8">
 
 <body>
+	<?php 		global $login_granted; $login_granted = false?>
+
 <!-- formulario de login -->
 <!-- utilizando um div principal contendo o formulario com os botoes e labels -->
 
 	<div class="class_divForm" id="id_divForm">
 
 
-		<form class="formLogin" role = "form" action="<? php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method = "post">
+		<form role = "form" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method = "post" id="divLoginFields" class="formLogin">
 
-			<label class="classErrorLabel opacity0" ><p><b>Erro!</b> Usuário inválido</p></label>
-			<input type="text" name="inputLogin" id="inputLogin" placeholder="Usuário" required autofocus><br>
-			<input type="password" name="inputPassword" id="inputPassword" placeholder="Senha" required><br>
+			<label class="classErrorLabel opacity0"><p>
+			<?php
+
+	            $msg = '';
+	            $login_granted = false;
+
+	            if (isset($_POST['username']) && !empty($_POST['username']) 
+	               && !empty($_POST['password'])) {
+					
+	               if ($_POST['username'] == 'admin' && 
+	                  $_POST['password'] == 'admin') {
+	                  $_SESSION['valid'] = true;
+	                  $_SESSION['timeout'] = time();
+	                  $_SESSION['username'] = 'tutorialspoint';
+	                  $msg = 'You have entered valid use name and password';
+	                  $login_granted = true;
+	               }else {
+	                  $msg = 'Wrong username or password';
+	               }
+	            }
+	            if ($msg != ""){
+					echo "$msg";
+	            }
+         	?>
+
+     </p></label>
+			<input type="text" name="username" id="inputLogin" placeholder="Usuário" required autofocus><br>
+			<input type="password" name="password" id="inputPassword" placeholder="Senha" required><br>
 			<input type="submit" name="inputSubmit" id="inputSubmit" value="Acessar"><br>
 			
 			<label id="labelEsqueceuSenha" class="classLabelInsideDivLogin"><a href="">Esqueceu a sua senha?</a></label><label id="labelCadastrese" class="classLabelInsideDivLogin">Não tem uma conta? <a href="">Cadastre-se</a>
 
 		</form>
+
 	</div>
 </body>
 </html>
