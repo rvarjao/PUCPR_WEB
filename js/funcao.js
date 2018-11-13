82e4164876d5c7432b4d28ce0944f8f0191eafb3
Guilherme Miranda <hahnmiranda@gmail.com>
2018-11-13 14:01:05 -0200

    Caixas Pronto

--- js/funcao.js
+++ js/funcao.js
@@ -1,33 +1,125 @@
 $(document).ready(function(){
-	
-	var musicas = [];
-	
-	$("#bAdicionarMusica").click(function(){
 
-		musicas.splice(0,0,$("#tNomeMusica").val());
+	fLocalCaixaDeEntrada();
 
-		document.getElementById("divListaMusicas").innerHTML="";
 
-		for (var i=0; i < musicas.length; i++) {
+	$("#bCaixaDeEntrada").click(function(){
+		fLocalCaixaDeEntrada();
+		return false;
+	});
 
-			$("#divListaMusicas").append("<div>" + musicas[i] + "</div>");
+	$("#bItensEnviados").click(function(){
+		fLocalCaixaDeSaida();
+		return false;
+	});
 
-		}
-		
-	})
-
-	$("#bRemoverMusica").click(function(){
-
-		musicas.splice(parseInt($("#tPosicaoRemover").val()), parseInt($("#tQuantidadeRemover").val()));
-
-		document.getElementById("divListaMusicas").innerHTML="";
-
-		for (var i=0; i < musicas.length; i++) {
-
-			$("#divListaMusicas").append("<div>" + musicas[i] + "</div>");
-
-		}
-
-	})
+	$("#bLixeira").click(function(){
+		fLocalLixeira();
+		return false;
+	});
 
 });
+
+function fLocalCaixaDeEntrada() {
+
+	$.ajax({
+		type: "POST",
+		dataType: "xml",
+		url: "emailsEntrada.xml",
+
+		success:function(data) {
+			
+			$("#caixAberta").children().remove();
+
+			$(data).find("Email").each( function() {
+
+				if($(this).find("Estado").text() == 1) {
+					var conteudo = '<div class="Linha">';
+					conteudo += '<div class="colMarcar"><input type="checkbox" name=""></div>';
+					conteudo += '<a href=""><div class="colEstado"><img src="../imagens/email_estado/trans/mail-icon.png"></div>';
+				}
+				if($(this).find("Estado").text() == 2) {
+					var conteudo = '<div class="Linha2">';
+					conteudo += '<div class="colMarcar"><input type="checkbox" name=""></div>';
+					conteudo += '<a href=""><div class="colEstado"><img src="../imagens/email_estado/trans/correio-envelope-de-volta-com-uma-folha-de-papel-no-interior_318-50434.png"></div>';
+				}
+
+				conteudo += '<div class="colRemetente"> ' + $(this).find("Remetente").text() + '</div>'; 
+				conteudo += '<div class="colEmail">' + $(this).find("EmailRemetente").text() + '</div>'; 
+				conteudo += '<div class="colAssunto">' + $(this).find("Assunto").text() + '</div>';  
+				conteudo += '<div class="colResumo">' + $(this).find("Resumo").text() + '</div></a></div>';
+
+				$("#caixAberta").append(conteudo);
+				
+			});
+		}
+	});
+}
+
+function fLocalCaixaDeSaida() {
+
+	$.ajax({
+
+		type: "POST",
+		dataType: "xml",
+		url: "emailsSaida.xml", 
+
+		success:function(data) {
+			
+			$("#caixAberta").children().remove();
+
+			$(data).find("Email").each( function() {
+
+				var conteudo = '<div class="Linha2">';
+				conteudo += '<div class="colMarcar"><input type="checkbox" name=""></div>';
+				conteudo += '<a href=""><div class="colEstado"><img src="../imagens/email_estado/trans/email_enviados.png"></div>';
+				conteudo += '<div class="colRemetente"> ' + $(this).find("Destinatario").text() + '</div>'; 
+				conteudo += '<div class="colEmail">' + $(this).find("EmailDestinatario").text() + '</div>'; 
+				conteudo += '<div class="colAssunto">' + $(this).find("Assunto").text() + '</div>';  
+				conteudo += '<div class="colResumo">' + $(this).find("Resumo").text() + '</div></a></div>';
+
+				$("#caixAberta").append(conteudo);
+
+			});
+		}
+	});
+
+}
+
+function fLocalLixeira() {
+
+	$.ajax({
+		type: "POST",
+		dataType: "xml",
+		url: "emailsLixeira.xml",
+
+		success:function(data) {
+			
+			$("#caixAberta").children().remove();
+
+			$(data).find("Email").each( function() {
+
+				var conteudo = '<div class="Linha2">';
+				conteudo += '<div class="colMarcar"><input type="checkbox" name=""></div>';
+
+				if ($(this).find("Estado").text() == 1){
+					conteudo += '<a href=""><div class="colEstado"><img src="../imagens/email_estado/trans/correio-envelope-de-volta-com-uma-folha-de-papel-no-interior_318-50434.png"></div>';
+				}
+
+				if ($(this).find("Estado").text() == 2){
+					conteudo += '<a href=""><div class="colEstado"><img src="../imagens/email_estado/trans/email_enviados.png"></div>';
+				}
+
+				conteudo += '<div class="colRemetente"> ' + $(this).find("Remetente").text() + '</div>'; 
+				conteudo += '<div class="colEmail">' + $(this).find("EmailRemetente").text() + '</div>'; 
+				conteudo += '<div class="colAssunto">' + $(this).find("Assunto").text() + '</div>';  
+				conteudo += '<div class="colResumo">' + $(this).find("Resumo").text() + '</div></a></div>';
+
+				$("#caixAberta").append(conteudo);
+
+			});
+		}
+	});
+
+}
+
